4922977e9fd1876a1c928f348c2ad2c993fbd42a
Abdelaziz Abubaker <32877991+AAbdelaziz00@users.noreply.github.com>
2023-03-08 19:19:46 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace AI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.btnDisable = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(22, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(238, 141);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.Location = new System.Drawing.Point(34, 207);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(75, 23);
+            this.btnEnable.TabIndex = 1;
+            this.btnEnable.Text = "Enable";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.btnEnable_Click);
+            // 
+            // btnDisable
+            // 
+            this.btnDisable.Enabled = false;
+            this.btnDisable.Location = new System.Drawing.Point(185, 207);
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.Size = new System.Drawing.Size(75, 23);
+            this.btnDisable.TabIndex = 2;
+            this.btnDisable.Text = "Disable";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(92)))), ((int)(((byte)(104)))));
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnDisable);
+            this.Controls.Add(this.btnEnable);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnEnable;
+        private System.Windows.Forms.Button btnDisable;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Speech.Recognition;
+using System.Windows.Forms;
+
+
+namespace AI{
+    public partial class Form1 : Form{
+
+        SpeechRecognitionEngine recognitionEngine = new SpeechRecognitionEngine();
+
+        private const string Dictonary_Path = @"C:\Users\User\Desktop\wordsList.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ArrayList WordDictonary = new ArrayList();
+ 
+                if (File.Exists(Dictonary_Path))
+                {
+                    using (StreamReader sr = File.OpenText(Dictonary_Path))
+                    {
+                        string word = "";
+                        while ((word = sr.ReadLine()) != null)
+                        {
+                            WordDictonary.Add(word);
+                        }
+                    }
+ 
+                }
+
+
+                string[] wordsToCOMMANDS = (String[])WordDictonary.ToArray(typeof(string));
+                
+
+                Choices commands = new Choices(wordsToCOMMANDS);
+                GrammarBuilder grBuilder = new GrammarBuilder();
+                grBuilder.Append(commands);
+
+                Grammar grammar = new Grammar(grBuilder);
+                recognitionEngine.LoadGrammar(grammar);
+
+                recognitionEngine.SetInputToDefaultAudioDevice();
+                recognitionEngine.SpeechRecognized += speecEngine_SpeechRecognized;
+
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        void speecEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e){
+            switch(e.Result.Text){
+                case "hello":
+                    richTextBox1.Text = "hello abdull";
+                    break;
+                case "print my name":
+                   MessageBox.Show("Hello Abdull");
+                    break;
+                case "close":
+                    Close();
+
+                    break;
+            }
+            
+        }
+        private void btnEnable_Click(object sender, EventArgs e)
+        {
+            recognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+            btnDisable.Enabled = true;
+            btnEnable.Enabled = false;
+        }
+
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            recognitionEngine.RecognizeAsyncStop();
+            btnDisable.Enabled = false;
+            btnEnable.Enabled = true;
+        }
+    }
+}
